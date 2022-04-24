e2050933c1d8b072517fa5fd00be7d15abeaf249
trungh2506 <trungvipqn12@gmail.com>
2022-04-24 11:33:27 +0700

    update dữ liệu phim

--- qnu-cinema.sql
+++ qnu-cinema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th4 22, 2022 lúc 04:42 AM
+-- Thời gian đã tạo: Th4 24, 2022 lúc 06:32 AM
 -- Phiên bản máy phục vụ: 10.4.22-MariaDB
 -- Phiên bản PHP: 8.1.2
 
@@ -85,7 +85,11 @@ CREATE TABLE `phim` (
 --
 
 INSERT INTO `phim` (`id_phim`, `ten_phim`, `the_loai`, `dao_dien`, `dien_vien`, `noi_dung`, `thoi_luong`, `do_tuoi`, `loai_phim`, `img`) VALUES
-(1, 'Doctor Strange 2', 'Hành động', 'Sam Raimi', 'Patrick Stewart, Benedict Cumberbatch, Elizabeth Olsen,..', 'a', 240, '', '', 'doctor.jpg');
+(1, 'DOCTOR STRANGE 2', 'Hành động', 'Sam Raimi', 'Patrick Stewart, Benedict Cumberbatch, Elizabeth Olsen,..', 'a', 240, 'C16', '2D', 'doctor.jpg'),
+(2, 'NGÔI ĐỀN KỲ QUÁI 3 (C16)', 'Bí Ẩn, Kinh Dị', 'Phontharis Chotkijsadarsopon', 'Phiravich Attachitsataporn, Sim Kyutae Pond, Khunnapat Pichetworawut', 'Hội tạo nghiệp tụ tập nhau đến dự lễ xuất gia của Aod nhưng phải hoãn lại do lời nguyền từ chiếc lắc chân xuất hiện. Min Jun, Balloon, First phải chạy đua với thời gian để tìm ra cách giải lời nguyền trước khi quỷ dữ đến lấy mạng Aod.', 180, 'C16', '2D', 'ngoi-den-ky-quai-3.jpg'),
+(3, 'TIẾNG “KÊU” CỨU LÚC NỬA ĐÊM (C18)', 'Hành Động, Tội Phạm', 'Kwon Oh-seung', 'Wi Ha-joon, Jin Ki-joo, Park Hoon, Kim Hye-yoon', 'Kyeong-mi là một cô gái khiếm thính làm việc tại một tổng đài hỗ trợ người khiếm thính. Một đêm đi làm về muộn, cô tình cờ chứng kiến So Jung-eun, một cô gái trẻ bị đâm tàn bạo trên con đường vắng. Từ giây phút đó, Kyeong-mi trở thành mục tiêu mới củ...', 180, 'C18', '2D', 'tieng-keu-cuu-luc-nua-dem.jpg'),
+(4, 'NGHỀ SIÊU DỄ (C16)', 'Hài', 'Võ Thanh Hòa', 'Hứa Vĩ Văn, Thu Trang, Kiều Minh Tuấn, Tiến Luật, Huỳnh Phương, Quang Tuấn, Vũ Ngọc Anh, Thanh Mỹ', 'Ông Thái là một cảnh sát về hưu nhưng không chịu an phận thủ thường, hàng ngày vẫn đi tìm bắt tội phạm vặt trong xóm cho đỡ nhớ nghề. Một ngày kia, Hoàng - tên trùm ma túy mới ra tù bỗng dưng chuyển đến xóm ông và mở một văn phòng bất động sản. Nghi ...', 180, 'C16', '2D', 'nghe-sieu-de.jpg'),
+(5, 'JUJUTSU KAISEN 0 - CHÚ THUẬT HỒI CHIẾN (C13)', 'Hoạt hình', 'Sunghoo Park', 'Hanazawa Kana, Nakamura Yuichi, Uchiyama Koki,…', 'Yuta Okkotsu, một học sinh trung học giành được quyền kiểm soát một Linh hồn bị nguyền rủa cực kỳ mạnh mẽ và được Jujutsu Sorcerers đăng ký vào trường trung học Jujutsu tỉnh Tokyo để giúp anh ta kiểm soát sức mạnh của mình và để mắt đến anh ta.', 180, 'C13', '2D', 'jujutsu.jpg');
 
 -- --------------------------------------------------------
 
@@ -173,7 +177,7 @@ ALTER TABLE `nhan_vien`
 -- AUTO_INCREMENT cho bảng `phim`
 --
 ALTER TABLE `phim`
-  MODIFY `id_phim` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_phim` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT cho bảng `tk_khach`
